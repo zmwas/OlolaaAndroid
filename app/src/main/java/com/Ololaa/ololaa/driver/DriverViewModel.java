25e1515dca13d5efc53f33f409e08bfda55c5531
Zacharia Njoroge <zacharianjoroge@Zacharias-MacBook-Pro-2.local>
2019-04-29 11:09:00 +0300

    Add driver registration layout

---
+++ app/src/main/java/com/Ololaa/ololaa/driver/DriverViewModel.java
@@ -0,0 +1,21 @@
+package com.Ololaa.ololaa.driver;
+
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+import android.databinding.ObservableField;
+
+public class DriverViewModel extends ViewModel {
+    public ObservableField<String> name = new ObservableField<>();
+    public ObservableField<String> errorName = new ObservableField<>();
+    public ObservableField<String> truck = new ObservableField<>();
+    public ObservableField<String> errorTruck = new ObservableField<>();
+    public ObservableField<String> drivingLicenseNumber = new ObservableField<>();
+    public ObservableField<String> errorDrivingLicenseNumber = new ObservableField<>();
+    public ObservableField<String> drivingLicenseType = new ObservableField<>();
+    public ObservableField<String> errorDrivingLicenseType = new ObservableField<>();
+    public ObservableField<String> idNumber = new ObservableField<>();
+    public ObservableField<String> erroridNumber = new ObservableField<>();
+    public MutableLiveData<Boolean> showPassportPhoto = new MutableLiveData<>();
+
+
+}
