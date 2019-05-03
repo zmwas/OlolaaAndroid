9ef1f99dc7154217ff7d2479c98d9cf7abc2b303
Zacharia Njoroge <zacharianjoroge@Zacharias-MacBook-Pro-2.local>
2019-05-03 13:08:12 +0300

    Empty truck layout

--- app/src/main/java/com/Ololaa/ololaa/trip/TripViewModel.java
+++ app/src/main/java/com/Ololaa/ololaa/trip/TripViewModel.java
@@ -5,6 +5,13 @@ import android.databinding.ObservableField;
 
 public class TripViewModel extends ViewModel {
     public ObservableField<String> truck = new ObservableField<>();
+    public ObservableField<String> truckType = new ObservableField<>();
+    public ObservableField<String> collectionPoint = new ObservableField<>();
+    public ObservableField<String> collectionDate = new ObservableField<>();
+    public ObservableField<String> transporter = new ObservableField<>();
+    public ObservableField<String> tonage = new ObservableField<>();
+    public ObservableField<String> kraPin = new ObservableField<>();
+    public ObservableField<Boolean> callMade = new ObservableField<>();
     public ObservableField<String> errorTruck = new ObservableField<>();
     public ObservableField<String> driver = new ObservableField<>();
     public ObservableField<String> errorDriver = new ObservableField<>();
