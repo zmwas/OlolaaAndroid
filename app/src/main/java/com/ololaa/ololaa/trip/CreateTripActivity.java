746105bbb912648354dfe66c3f2c149944c6c42d
Zacharia Njoroge <zacharianjoroge@Zacharias-MacBook-Pro-2.local>
2019-05-18 11:21:35 +0300

    Add activities

---
+++ app/src/main/java/com/ololaa/ololaa/trip/CreateTripActivity.java
@@ -0,0 +1,6 @@
+package com.ololaa.ololaa.trip;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class CreateTripActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/ololaa/ololaa/user/RegisterUserActivity.java
@@ -0,0 +1,6 @@
+package com.ololaa.ololaa.user;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class RegisterUserActivity extends AppCompatActivity {
+}
