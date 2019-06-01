02a2fca21b0cff67a86cc504dd3f364052266653
Zacharia Njoroge <zacharianjoroge@Zacharias-MacBook-Pro-2.local>
2019-06-01 15:18:52 +0300

    Add binding

--- app/src/main/java/com/ololaa/ololaa/booking/FilterTripsActivity.java
+++ app/src/main/java/com/ololaa/ololaa/booking/FilterTripsActivity.java
@@ -1,12 +1,15 @@
 package com.ololaa.ololaa.booking;
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.ololaa.ololaa.OlolaaViewModelFactory;
+import com.ololaa.ololaa.R;
 import com.ololaa.ololaa.common.GPSLiveData;
+import com.ololaa.ololaa.databinding.ActivityFilterTripBinding;
 
 import javax.inject.Inject;
 
@@ -15,13 +18,15 @@ public class FilterTripsActivity extends AppCompatActivity {
     @Inject
     OlolaaViewModelFactory factory;
     BookingViewModel viewModel;
+    ActivityFilterTripBinding binding;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_filter_trip);
         gpsLiveData = new GPSLiveData(this);
         viewModel = ViewModelProviders.of(this, factory).get(BookingViewModel.class);
+        binding.setViewModel(viewModel);
         gpsLiveData.observe(this, locationStatus -> {
             if (locationStatus.getErrorMessage() != null) {
 
